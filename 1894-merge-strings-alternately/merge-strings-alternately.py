3f81b0f6044877b71293684ae8a18a31ecdad825
yenting_lee <leeyt1377@gmail.com>
2024-12-08 11:58:42 +0100

    Time: 14 ms (61.57%) | Memory: 12.3 MB (8.45%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result=""
+        if len(word1)==len(word2):
+            for i in range(len(word1)):
+                result = result + word1[i] + word2[i]
+        elif len(word1) < len(word2):
+            for i in range(len(word1)):
+                result = result + word1[i] + word2[i]
+            result += word2[i+1:]
+        else:
+            for i in range(len(word2)):
+                result = result + word1[i] + word2[i]
+            result += word1[i+1:]
+        
+        #result = ''.join(sorted(set(result), key=result.index))
+        return result
+            
+            
+                
+        
